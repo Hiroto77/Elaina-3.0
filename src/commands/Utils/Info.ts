de514a82c22745e4cb768880f02465c19f398790
Sujal-Elaina <89565759+Sujal-Elaina@users.noreply.github.com>
2021-11-26 17:37:25 +0530

    Update Info.ts

--- src/commands/Utils/Info.ts
+++ src/commands/Utils/Info.ts
@@ -17,8 +17,7 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const chats: any = this.client.chats
 			
-		await M.reply(`━━━━❰ 💙ELAINA💙 ❱━━━━\n\n🔮Groups: *${this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length}\n\n *🚦Uptime: ${uptime()} ( 
-const  formatTime = (seconds) => {
+		await M.reply(`━━━━❰ 💙ELAINA💙 ❱━━━━\n\n🔮Groups: *${this.client.chats.all().filter(chat => chat.jid.endsWith('g.us')).length}\n\n *🚦Uptime: ${uptime()} const  formatTime = (seconds) => {
 
         const hrs = Math.floor(seconds / (60 * 60))
 
@@ -33,10 +32,6 @@ const  formatTime = (seconds) => {
 const pad = (s) => (s < 10 ? '0' : '') + s
 
 const uptime = () => formatTime(process.uptime()) 
-        
-    }
-
-
-
+       
     }
 }
