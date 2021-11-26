f0083d60dc9c99b137f82bd94ac2bd2ea10227eb
Sujal-Elaina <89565759+Sujal-Elaina@users.noreply.github.com>
2021-11-26 12:55:52 +0530

    Update and rename src/commands/Moderation/Everyone.ts to src/commands/moderation/all.ts

--- src/commands/moderation/all.ts
+++ src/commands/moderation/all.ts
@@ -18,7 +18,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         return void (await M.reply(
-            `${M.groupMetadata?.subject || '*EVERYONE*'}\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
+            `${M.groupMetadata?.subject || '*EVERYONE*'}\n*READ MY TAGGED MSG*\n*[ANNOUNCEMENT 🚀]*`,
             undefined,
             undefined,
             M.groupMetadata?.participants.map((user) => user.jid)
