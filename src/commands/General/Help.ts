4820c3c053c39e5573097ebf34dde993b0651b59
Sujal-Elaina <89565759+Sujal-Elaina@users.noreply.github.com>
2021-11-26 11:23:09 +0530

    Add files via upload

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -20,8 +20,9 @@ export default class Command extends BaseCommand {
 		M: ISimplifiedMessage,
 		parsedArgs: IParsedArgs
 	): Promise<void> => {
-		const n = ["./assets/videos/Chitoge/chitoge.mp4"];
-		const chitoge = n[Math.floor(Math.random() * n.length)];
+		const user = M.sender.jid;
+		const chitoge =
+			"https://media.tenor.com/videos/571d88ea5d66e7b95cdbc4ef6029dd95/mp4";
 		if (!parsedArgs.joined) {
 			const commands = this.handler.commands.keys();
 			const categories: { [key: string]: ICommand[] } = {};
@@ -29,6 +30,12 @@ export default class Command extends BaseCommand {
 				const info = this.handler.commands.get(command);
 				if (!command) continue;
 				if (!info?.config?.category || info.config.category === "dev") continue;
+				if (
+					!info?.config?.category ||
+					(info.config.category === "nsfw" &&
+						!(await this.client.getGroupData(M.from)).nsfw)
+				)
+					continue;
 				if (Object.keys(categories).includes(info.config.category))
 					categories[info.config.category].push(info);
 				else {
@@ -36,25 +43,30 @@ export default class Command extends BaseCommand {
 					categories[info.config.category].push(info);
 				}
 			}
-			let text = `*Hello👋@${M.sender.username}*.I'm ${
-                this.client.user.notify || this.client.user.vname || this.client.user.name || this.client.config.name
-            },a bot to make_your_Work_easy                       \n\n`
-            const keys = Object.keys(categories)
-            for (const key of keys)
-                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}* ${this.emojis[keys.indexOf(key)]}\n\n• \`\`\`${categories[
-                    key
-                ]
-                    .map((command) => command.config?.command)
-                    .join(' \n ')}\`\`\`\n\n`
-            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {quoted:M.WAMessage,
-            mimetype: Mimetype.gif,
-            caption: `${text} 
-
-              ♾️ Elaina ♾️
-
-           🎗 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
-            )
-        }
+			let text = `👋🏻 (💙ω💙) Konichiwa! *@${
+				user.split("@")[0]
+			}*, I'm Chitoge.\n\nMy prefix is - "${
+				this.client.config.prefix
+			}"\n\nThe usable commands are listed below.\n\n`;
+			const keys = Object.keys(categories);
+			for (const key of keys)
+				text += `${
+					this.emojis[keys.indexOf(key)]
+				} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[key]
+					.map((command) => command.config?.command)
+					.join(" \n ")}\`\`\`\n\n`;
+			return void this.client.sendMessage(
+				M.from,
+				{ url: chitoge },
+				MessageType.video,
+				{
+					quoted: M.WAMessage,
+					mimetype: Mimetype.gif,
+					caption: `${text} 📝 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`,
+					contextInfo: { mentionedJid: [user] },
+				}
+			);
+		}
 		const key = parsedArgs.joined.toLowerCase();
 		const command =
 			this.handler.commands.get(key) || this.handler.aliases.get(key);
@@ -83,5 +95,5 @@ export default class Command extends BaseCommand {
 		);
 	};
 
-	emojis = ["❄", "📚", "🌈", "♟", "📍", "♻️", "🎵", "👑", "🚫", "✨"];
+	emojis = ["❄", "📚", "🌈", "♟", "📍", "🎵", "👑", "🚫", "♻️", "✨"];
 }
