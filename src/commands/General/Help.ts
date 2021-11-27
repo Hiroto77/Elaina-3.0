76360c9d2d4fb73a3b8561d560e3417071866251
Sujal-Elaina <89565759+Sujal-Elaina@users.noreply.github.com>
2021-11-27 10:44:22 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -39,16 +39,66 @@ export default class Command extends BaseCommand {
 			let text = `*Hello👋@${M.sender.username}*.I'm ${
                 this.client.user.notify || this.client.user.vname || this.client.user.name || this.client.config.name
             },a bot to make_your_Work_easy                       \n\n`
-            const keys = Object.keys(categories)
-            for (const key of keys)
-                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}* ${this.emojis[keys.indexOf(key)]}\n\n• \`\`\`${categories[
-                    key
-                ]
-                    .map((command) => command.config?.command)
-                    .join(' \n ')}\`\`\`\n\n`
-            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {quoted:M.WAMessage,
-            mimetype: Mimetype.gif,
-            caption: `${text} 
+            const keys = 
+🄰🄽🄸🄼🄴 〽
+- ${this.client.config.prefix}𝙖𝙣𝙞𝙢𝙚𝙦𝙪𝙤𝙩𝙚 
+- ${this.client.config.prefix}𝙖𝙣𝙞𝙢𝙚𝙘𝙝𝙖𝙧 
+- ${this.client.config.prefix}𝙝𝙪𝙨𝙗𝙖𝙣𝙙𝙤 
+- ${this.client.config.prefix}𝙡𝙤𝙡𝙞 
+- ${this.client.config.prefix}𝙣𝙚𝙠𝙤 
+- ${this.client.config.prefix}𝙬𝙖𝙞𝙛𝙪
+🄼🄴🄳🄸🄰 〽
+- ${this.client.config.prefix}𝙥𝙡𝙖𝙮 
+- ${this.client.config.prefix}𝙨𝙥𝙤𝙩𝙞𝙛𝙮 
+- ${this.client.config.prefix}𝙮𝙩𝙖𝙪𝙙𝙞𝙤 
+- ${this.client.config.prefix}𝙮𝙩𝙨𝙚𝙖𝙧𝙘𝙝 
+- ${this.client.config.prefix}𝙮𝙩𝙫𝙞𝙙𝙚𝙤 
+- ${this.client.config.prefix}𝙜𝙤𝙤𝙜𝙡𝙚 
+- ${this.client.config.prefix}𝙡𝙮𝙧𝙞𝙘𝙨
+🄵🅄🄽 〽
+- ${this.client.config.prefix}𝙗𝙤𝙣𝙠 
+- ${this.client.config.prefix}𝙘𝙝𝙚𝙨𝙨 
+- ${this.client.config.prefix}𝙧𝙚𝙖𝙘𝙩 
+- ${this.client.config.prefix}𝙩𝙧𝙞𝙜𝙜𝙚𝙧 
+- ${this.client.config.prefix}𝙘𝙝𝙖𝙩 
+- ${this.client.config.prefix}𝙛𝙖𝙘𝙩 
+- ${this.client.config.prefix}𝙧𝙥𝙖𝙥𝙚𝙧 
+- ${this.client.config.prefix}𝙨𝙖𝙛𝙚𝙟𝙤𝙠𝙚 
+- ${this.client.config.prefix}𝙨𝙝𝙞𝙥
+🄶🄴🄽🄴🅁🄰🄻 〽
+- ${this.client.config.prefix}𝙖𝙙𝙢𝙞𝙣𝙨 
+- ${this.client.config.prefix}𝙚𝙫𝙚𝙧𝙮𝙤𝙣𝙚 
+- ${this.client.config.prefix}𝙝𝙚𝙡𝙥 
+- ${this.client.config.prefix}𝙢𝙤𝙙𝙨 
+- ${this.client.config.prefix}𝙥𝙧𝙤𝙛𝙞𝙡𝙚 
+- ${this.client.config.prefix}𝙍𝙖𝙣𝙠
+- ${this.client.config.prefix}𝙞𝙣𝙫𝙞𝙩𝙚𝙡𝙞𝙣𝙠 
+- ${this.client.config.prefix}𝙨𝙘𝙧𝙚𝙚𝙣𝙨𝙝𝙤𝙩 
+- ${this.client.config.prefix}𝙙𝙚𝙡𝙚𝙩𝙚
+🄼🄸🅂🄲 〽
+- ${this.client.config.prefix}𝙧𝙚𝙩𝙧𝙞𝙚𝙫𝙚 
+- ${this.client.config.prefix}𝙝𝙞 
+- ${this.client.config.prefix}𝙮𝙖𝙩𝙤𝙧𝙞
+🄼🄾🄳🄴🅁🄰🅃🄸🄾🄽 〽
+- ${this.client.config.prefix}𝙖𝙘𝙩𝙞𝙫𝙖𝙩𝙚 
+- ${this.client.config.prefix}𝙙𝙚𝙖𝙘𝙩𝙞𝙫𝙖𝙩𝙚 
+- ${this.client.config.prefix}𝙙𝙚𝙢𝙤𝙩𝙚 
+- ${this.client.config.prefix}𝙥𝙧𝙤𝙢𝙤𝙩𝙚 
+- ${this.client.config.prefix}𝙥𝙪𝙧𝙜𝙚 
+- ${this.client.config.prefix}𝙧𝙚𝙢𝙤𝙫𝙚 
+- ${this.client.config.prefix}𝙘𝙡𝙤𝙨𝙚 
+- ${this.client.config.prefix}𝙤𝙥𝙚𝙣 
+- ${this.client.config.prefix}𝙧𝙚𝙫𝙤𝙠𝙚
+🅄🅃🄸🄻🅂 〽
+- ${this.client.config.prefix}𝙗𝙡𝙪𝙧 
+- ${this.client.config.prefix}𝙨𝙩𝙞𝙘𝙠𝙚𝙧 
+- ${this.client.config.prefix}𝙨𝙪𝙗𝙧𝙚𝙙 
+- ${this.client.config.prefix}𝙨𝙩𝙚𝙖𝙡
+🄴🄳🅄🄲🄰🅃🄸🅅🄴 〽
+- ${this.client.config.prefix}𝙘𝙤𝙫𝙞𝙙 
+- ${this.client.config.prefix}𝙙𝙚𝙛𝙞𝙣𝙚 
+- ${this.client.config.prefix}𝙜𝙞𝙩𝙝𝙪𝙗 
+🍁 𝙉𝙤𝙩𝙚: 𝙐𝙨𝙚 !𝙝𝙚𝙡𝙥 <𝙘𝙤𝙢𝙢𝙖𝙣𝙙_𝙣𝙖𝙢𝙚> 𝙩𝙤 𝙫𝙞𝙚𝙬 𝙩𝙝𝙚 𝙘𝙤𝙢𝙢𝙖𝙣𝙙 𝙞𝙣𝙛𝙤.` }
 
               ♾️ Elaina ♾️
 
@@ -82,6 +132,4 @@ export default class Command extends BaseCommand {
 			}`
 		);
 	};
-
-	emojis = ["❄", "📚", "🌈", "♟", "📍", "♻️", "🎵", "👑", "🚫", "✨"];
 }
